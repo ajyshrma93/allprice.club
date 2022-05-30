0d9cb9c7e90a11147e4d8150ddf1b277e4f8b341
ajyshrma93 <ajyshrma69@gmail.com>
2022-05-30 20:16:36 +0530

    update layout

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -28,7 +28,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="card">
-                    <div class="card-body">
+                    <div class="card-body p-2">
                         <div class="row">
                             <div class="col-xl-6 col-8 mb-3 col-xxl-7 col-xl-6 col-md-8 ">
                                 <select class="form-select select2" id="filter_shop">
