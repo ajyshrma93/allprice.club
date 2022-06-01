354b577e9db934caf1d806fd0cc50b62db30ea3e
ajyshrma93 <ajyshrma69@gmail.com>
2022-06-01 09:53:46 +0530

    added report page and also added filter in shop list

--- resources/views/reports/partials/list.blade.php
+++ resources/views/reports/partials/list.blade.php
@@ -9,7 +9,6 @@ $date= $product->created_at->format('Y-m-d');
         </button>
         | RM (<?= \App\Models\Product::where('user_id', auth()->id())->whereDate('created_at', $product->created_at)->sum('price') ?>)
         | Shop : {{$product->shop ? $product->shop->name :''}}
-        {{$product->created_at}}
     </div>
 </div>
 @endforeach
