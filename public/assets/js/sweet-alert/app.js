6563475485beaba6af65712d56342fe9a22e0b47
ajyshrma93 <ajyshrma69@gmail.com>
2022-06-01 21:56:58 +0530

    shop delete fixed

--- public/assets/js/sweet-alert/app.js
+++ public/assets/js/sweet-alert/app.js
@@ -1,15 +1,13 @@
-document.querySelectorAll(".delete-product").forEach(function (e) {
-    e.onclick = function () {
-        swal({
-            title: "Are you sure?",
-            text: "Once deleted, you will not be able to recover this imaginary file!",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-        }).then((willDelete) => {
-            if (willDelete) {
-                $(this).parent("form").submit();
-            }
-        });
-    };
+$("body").on("click", ".delete-product", function () {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    }).then((willDelete) => {
+        if (willDelete) {
+            $(this).parent("form").submit();
+        }
+    });
 });
