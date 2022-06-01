2edd179b27dd715010a149f71b8c0dbc7cf5ff98
ajyshrma93 <ajyshrma69@gmail.com>
2022-06-01 21:59:05 +0530

    report page fix

--- resources/views/reports/partials/list.blade.php
+++ resources/views/reports/partials/list.blade.php
@@ -1,18 +1,19 @@
 @foreach ($products as $product)
 @php
 $shop = $product->shop;
+$image = $shop ? $shop->image :'assets/images/no-data-available.png';
 @endphp
 <div class="col-xl-3 col-sm-6">
     <div class="card">
         <div class="product-box">
-            <div class="product-img" style="background-image: url('{{asset($shop->image)}}');background-size: 100% 100%;">
+            <div class="product-img" style="background-image: url('{{asset($image)}}');background-size: 100% 100%;">
                 <div class="ribbon ribbon-info ribbon-right">{{$product->total_items}} Items</div>
                 <div class="product-hover">
 
                 </div>
             </div>
             <div class="product-details">
-                <h4>{{$shop->name}}</h4>
+                <h4>{{ $shop ? $shop->name :'N/A'}}</h4>
                 <div class="product-price">
                     $ {{$product->total_price}}
                 </div>
