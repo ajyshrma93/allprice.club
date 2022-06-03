d4742240c17bdae7afba2d6eee8c208ca2253e74
ajyshrma93 <ajyshrma69@gmail.com>
2022-06-03 10:33:33 +0530

    update report change

--- resources/views/reports/partials/list.blade.php
+++ resources/views/reports/partials/list.blade.php
@@ -5,22 +5,23 @@ $image = $shop ? $shop->image :'assets/images/no-data-available.png';
 @endphp
 <div class="col-xl-3 col-sm-6 col-6">
     <div class="card">
-        <a href="{{route('reports.details',$product->id)}}">
-            <div class="product-box">
+        <div class="product-box">
+            <a href="{{route('reports.details',$product->id)}}">
+
                 <div class="product-img" style="background-image: url('{{asset($image)}}');background-size: 100% 100%;">
                     <div class="ribbon ribbon-info ribbon-right">{{$product->total_items}} Items</div>
                     <div class="product-hover">
 
                     </div>
                 </div>
-                <div class="product-details">
-                    <h4>{{ $shop ? $shop->name :'N/A'}}</h4>
-                    <div class="product-price"> RM {{$product->total_price}}<br />
-                        <span>{{$product->created_at->format('d/m/Y')}}</span>
-                    </div>
+            </a>
+            <div class="product-details">
+                <h4>{{ $shop ? $shop->name :'N/A'}}</h4>
+                <div class="product-price"> RM {{$product->total_price}}<br />
+                    <span>{{$product->created_at->format('d/m/Y')}}</span>
                 </div>
             </div>
-        </a>
+        </div>
     </div>
 </div>
 @endforeach
